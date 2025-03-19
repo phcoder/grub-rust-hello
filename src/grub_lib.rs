7dc15605f44e0991e5cfb8dc4743aa3de1a21518
Vladimir Serbinenko <phcoder@gmail.com>
2025-03-19 16:15:47 +0300

    Working with regparm=0

--- src/grub_lib.rs
+++ src/grub_lib.rs
@@ -1,18 +1,40 @@
 extern crate alloc;
+
+use core::alloc::{GlobalAlloc, Layout};
+use core::ffi::c_char;
+
 use alloc::ffi::CString;
 use core::panic::PanicInfo;
 
 extern "C" {
-    static grub_xputs: extern "C" fn(stri: *const u8);
+    static grub_xputs: extern "C" fn(stri: *const c_char);
     pub fn grub_abort();
+    pub fn grub_malloc(sz: usize) -> *mut u8;
+    pub fn grub_free(ptr: *mut u8);
 }
 
+
+struct GrubAllocator;
+
+unsafe impl GlobalAlloc for GrubAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        return grub_malloc(layout.size());
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        grub_free(ptr);
+    }
+}
+
+#[global_allocator]
+static GLOBAL: GrubAllocator = GrubAllocator;
+
 pub fn xputs(val: &str) {
-/*    let c_to_print = CString::new(val).unwrap();
+    let c_to_print = CString::new(val).unwrap();
 
     unsafe {
 	grub_xputs(c_to_print.as_ptr());
-    }*/
+    }
 }
 
 #[panic_handler]
