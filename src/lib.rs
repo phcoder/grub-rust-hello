4fe1f2185a660600519d5a7e72676865d0e2b4bc
Vladimir Serbinenko <phcoder@gmail.com>
2025-03-21 17:10:07 +0300

    Rename dprint conditional

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@ pub static GRUB_LICENSE: [u8; 15] = *b"LICENSE=GPLv3+\0";
 
 pub fn rust_hello (argv: &[&str]) -> grub_lib::ErrT {
     println!("Hello, world argv={argv:?}");
-    dprintln!("rust", "hello from debug");
+    dprintln!("hello", "hello from debug");
     return 0;
 }
 
