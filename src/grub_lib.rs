4cc03faf398f91d8d9fb46755dab28a217bf3847
Vladimir Serbinenko <phcoder@gmail.com>
2025-03-22 16:19:00 +0300

    Add file interface

--- src/grub_lib.rs
+++ src/grub_lib.rs
@@ -21,7 +21,7 @@ extern "C" {
     fn grub_free(ptr: *mut u8);
     fn grub_register_command_prio (name: *const c_char,
 				   func: extern "C" fn (cmd: *const GrubCommand,
-							argc: c_int, argv: *const *const c_char) -> u32,
+							argc: c_int, argv: *const *const c_char) -> ErrT,
 				   summary: *const c_char,
 				   description: *const c_char,
 				   prio: c_int) -> *mut GrubCommand;
@@ -29,7 +29,15 @@ extern "C" {
     fn grub_unregister_command (cmd: *const GrubCommand);
     fn grub_refresh ();
     fn grub_debug_enabled(cond: *const c_char) -> bool;
-    fn grub_error (n: u32, fmt: *const c_char, args: ...) -> u32;
+    fn grub_error (n: ErrT, fmt: *const c_char, args: ...) -> ErrT;
+
+    fn grub_file_open (name: *const c_char, typ: FileType) -> *mut GrubFile;
+    fn grub_file_read (fil: *mut GrubFile, buf: *mut c_void, len: usize) -> isize;
+    fn grub_file_seek (fil: *mut GrubFile, offset: u64) -> u64;
+    fn grub_file_close (fil: *mut GrubFile);
+
+    static grub_errno: ErrT;
+    static grub_errmsg: *const c_char;
 }
 
 #[macro_export]
@@ -99,7 +107,45 @@ struct GrubCommand {
     data: *const c_void,
 }
 
+#[repr(C)]
+struct GrubFile {
+    /* File name.  */
+    name: *const c_char,
+
+    /* The underlying device.  */
+    device: *const c_void,
+
+    /* The underlying filesystem.  */
+    fs: *const c_void,
+
+    /* The current offset.  */
+    offset: u64,
+    progress_offset: u64,
+
+    /* Progress info. */
+    last_progress_time: u64,
+    last_progress_offset: u64,
+    estimated_speed: u64,
+
+    /* The file size.  */
+    size: u64,
+
+    /* If file is not easily seekable. Should be set by underlying layer.  */
+    not_easily_seekable: bool,
+
+    /* Filesystem-specific data.  */
+    data: *const c_void,
+
+    /* This is called when a sector is read. Used only for a disk device.  */
+    read_hook: *const c_void,
+
+    /* Caller-specific data passed to the read hook.  */
+    read_hook_data: *const c_void,
+}
+
 // TODO: Use codegen here
+#[derive(Clone)]
+#[repr(u32)]
 pub enum ErrT {
     None = 0,
     TestFailure = 1,
@@ -152,14 +198,101 @@ pub enum ErrT {
     RecursionDepth = 48,
 }
 
+#[derive(Clone)]
+#[repr(u32)]
+pub enum FileType {
+    None = 0,
+    GrubModule = 1,
+    Loopback = 2,
+    LinuxKernel = 3,
+    LinuxInitrd = 4,
+
+    MultibootKernel = 5,
+    MultibootModule = 6,
+
+    XenHypervisor = 7,
+    XenModule = 8,
+
+    BsdKernel = 9,
+    FreebsdEnv = 10,
+    FreebsdModule = 11,
+    FreebsdModuleElf = 12,
+    NetbsdModule = 13,
+    OpenbsdRamdisk = 14,
+
+    XnuInfoPlist = 15,
+    XnuMkext = 16,
+    XnuKext = 17,
+    XnuKernel = 18,
+    XnuRamdisk = 19,
+    XnuHibernateImage = 20,
+    GrubFileXnuDevprop = 21,
+
+    Plan9Kernel = 22,
+
+    Ntldr = 23,
+    Truecrypt = 24,
+    Freedos = 25,
+    Pxechainloader = 26,
+    Pcchainloader = 27,
+
+    CorebootChainloader = 28,
+
+    EfiChainloadedImage = 29,
+
+    Signature = 30,
+    PublicKey = 31,
+    PublicKeyTrust = 32,
+    PrintBlocklist = 33,
+    Testload = 34,
+    GetSize = 35,
+    Font = 36,
+    ZfsEncryptionKey = 37,
+    CryptodiskEncryptionKey = 38,
+    CryptodiskDetachedHeader = 39,
+    Fstest = 40,
+    Mount = 41,
+    FileId = 42,
+    AcpiTable = 43,
+    DeviceTreeImage = 44,
+    Cat = 45,
+    Hexcat = 46,
+    Cmp = 47,
+    Hashlist = 48,
+    ToHash = 49,
+    KeyboardLayout = 50,
+    Pixmap = 51,
+    GrubModuleList = 52,
+    Config = 53,
+    Theme = 54,
+    GettextCatalog = 55,
+    FsSearch = 56,
+    Audio = 57,
+    VbeDump = 58,
+
+    Loadenv = 59,
+    Saveenv = 60,
+
+    VerifySignature = 61,
+
+    Mask = 0xffff,
+
+    SkipSignature = 0x10000,
+    NoDecompress = 0x20000
+}
+
 pub struct GrubError {
     errno: ErrT,
     errmsg: String,
 }
 
 impl GrubError {
-    pub fn new(num: ErrT, msg: &str) -> Self {
-	return GrubError{errno: num, errmsg: msg.to_string()};
+    pub fn new(num: &ErrT, msg: &str) -> Self {
+	return GrubError{errno: num.clone(), errmsg: msg.to_string()};
+    }
+
+    pub fn from_env() -> Self {
+	return GrubError{errno: unsafe { grub_errno.clone() }, errmsg: unsafe { CStr::from_ptr(grub_errmsg) }.to_str().unwrap().to_string()};
     }
 
     pub fn new_fmt(num: ErrT, args: Arguments<'_>) -> Self {
@@ -203,18 +336,18 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 extern "C" fn cmd_callback (cmd: *const GrubCommand,
-			    argc: c_int, argv: *const *const c_char) -> u32 {
+			    argc: c_int, argv: *const *const c_char) -> ErrT {
     let mut argv_vec: Vec<&str> = vec![];
     for i in 0..argc {
 	argv_vec.push(unsafe { CStr::from_ptr(*argv.add(i as usize)) }.to_str().unwrap());
     }
     let f = unsafe { *((&(*cmd).data) as *const _ as *const fn(&[&str]) -> Result<(), GrubError>) };
     match f (&argv_vec) {
-	Ok(_) => { return 0; }
+	Ok(_) => { return ErrT::None; }
 	Err(e) => {
 	    let c_errmsg = CString::new(e.errmsg).unwrap();
 
-	    return unsafe {grub_error(e.errno as u32, CStr::from_bytes_until_nul(b"%s\0").unwrap().as_ptr(), c_errmsg.as_ptr())};
+	    return unsafe {grub_error(e.errno, CStr::from_bytes_until_nul(b"%s\0").unwrap().as_ptr(), c_errmsg.as_ptr())};
 	}
     }
 }
@@ -291,4 +424,45 @@ pub fn real_dprintln(file: &str, line: u32, cond: &str, args: Arguments<'_>) {
     print!("{file}:{line}:{cond}: ");
     print_fmt(args);
     unsafe { grub_refresh (); }
+}
+
+struct File {
+    file: *mut GrubFile,
+}
+
+impl File {
+    pub fn open(fname: &str, file_type: &FileType) -> Result<Self, GrubError> {
+	let c_name = CString::new(fname).unwrap();
+	let ret = unsafe{ grub_file_open(c_name.as_ptr(), file_type.clone()) };
+	if ret == core::ptr::null_mut() {
+	    return Err(GrubError::from_env());
+	}
+	return Ok(File { file: ret });
+    }
+
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize, GrubError> {
+	let ret = unsafe { grub_file_read(self.file, buf.as_mut_ptr() as *mut c_void, buf.len()) };
+	if ret < 0 {
+	    return Err(GrubError::from_env());
+	}
+	return Ok(ret as usize);
+    }
+
+    fn seek(&mut self, off: u64) {
+	unsafe { grub_file_seek(self.file, off) };
+    }
+
+    fn tell(&self) -> u64 {
+	return unsafe { (*self.file).offset };
+    }
+
+    fn size(&self) -> u64 {
+	return unsafe { (*self.file).size };
+    }
+}
+
+impl Drop for File {
+    fn drop(&mut self) {
+	unsafe { grub_file_close(self.file); }
+    }
 }
